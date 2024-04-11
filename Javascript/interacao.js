2fbf7b583c47f7c6c36ca2ecbda7d5b9fd881a51
Enzoo-sa <enzosa90@gmail.com>
2024-04-11 12:46:12 -0300

    first commit

---
+++ Javascript/interacao.js
@@ -0,0 +1,5 @@
+function enviar(){
+    var env = document.querySelector('.preencher')
+
+    env.innerHTML = '<h2 style="line-height: 150px">ENVIADO! &#x1F680 OBRIGADO POR SUA PARTICIPAÇÃO! &#x1F60D</h2>'
+}
